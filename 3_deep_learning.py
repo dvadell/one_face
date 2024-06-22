612930a95de112ad227d338656f0bc67b28e46cc
Your Name <you@example.com>
2024-06-22 17:58:52 +0000

    Cover the screen with a fullscreen screenshot

--- 3_deep_learning.py
+++ 3_deep_learning.py
@@ -3,6 +3,7 @@ import numpy as np
 
 # Load the model
 # Download these files from OpenCV's GitHub repo or somewhere else
+threshold_conf = 0.7
 model = "res10_300x300_ssd_iter_140000.caffemodel"
 net = cv2.dnn.readNetFromCaffe('deploy.prototxt', model) 
 
@@ -18,9 +19,13 @@ while True:
     net.setInput(blob)
     detections = net.forward()
 
+    # How many faces?
+    num_faces = 0
+
     for i in range(detections.shape[2]): # For each detected face
         confidence = detections[0, 0, i, 2]
-        if confidence > 0.5: # Filter out weak detections by a threshold of 0.5
+        if confidence > threshold_conf: # Filter out weak detections by a threshold of 0.5
+            num_faces = num_faces + 1
             box = detections[0, 0, i, 3:7] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
             (startX, startY, endX, endY) = box.astype("int")
             
@@ -28,6 +33,13 @@ while True:
             cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
     
     cv2.imshow('Video', frame)
+
+    if num_faces > 1:
+        cv2.namedWindow('2 faces!', cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty('2 faces!', cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        cv2.imshow('2 faces!', frame)
+        cv2.waitKey(3000000)
+        break 
     
     if cv2.waitKey(1) & 0xFF == ord('q'): # Press 'q' to quit the program
         break
